5b8c9922277a91d376f0e98d691488660aadc0cc
sun <sunhanshin2@gmail.com>
2021-04-18 13:58:27 -0500

    install bootstrap-vue and remove theme

--- src/views/ProfessorsIndex.vue
+++ src/views/ProfessorsIndex.vue
@@ -1,52 +1,49 @@
 <template>
   <div class="professors-index">
-    <div class="container">
-      <div>
-        <br />
-        Search Professors: <input type="text" v-model="professorSearch" />
+    <div>
+      <br />
+      Search Professors: <input type="text" v-model="professorSearch" />
+    </div>
+    <div class="mt-3">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
       </div>
-      <br /><br />
-      <div class="col-md-12">
-        <span class="border">
-          <div
-            class="col-sm-4 margin20"
-            v-for="professor in filterBy(
-              professors,
-              professorSearch,
-              'name',
-              'university'
-            )"
-          >
-            <h4>
-              <router-link :to="`/professors/${professor.id}`">{{
-                professor.name
-              }}</router-link>
-            </h4>
-            <p>University: {{ professor.university }}</p>
-            <p>Department: {{ professor.department }}</p>
-            <p>Rating: {{ avgRating(professor.reviews) }}</p>
-            <br />
-          </div>
-        </span>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
       </div>
-      <!-- <div
-        v-for="professor in filterBy(
-          professors,
-          professorSearch,
-          'name',
-          'university'
-        )"
-      >
-        <h4>
-          <router-link :to="`/professors/${professor.id}`">{{
-            professor.name
-          }}</router-link>
-        </h4>
-        <p>University: {{ professor.university }}</p>
-        <p>Department: {{ professor.department }}</p>
-        <p>Rating: {{ avgRating(professor.reviews) }}</p>
-        <br />
-      </div> -->
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+    <div
+      v-for="professor in filterBy(
+        professors,
+        professorSearch,
+        'name',
+        'university'
+      )"
+    >
+      <h4>
+        <router-link :to="`/professors/${professor.id}`">{{
+          professor.name
+        }}</router-link>
+      </h4>
+      <p>University: {{ professor.university }}</p>
+      <p>Department: {{ professor.department }}</p>
+      <p>Rating: {{ avgRating(professor.reviews) }}</p>
+      <br />
     </div>
   </div>
 </template>
--- src/views/ReviewsNew.vue
+++ src/views/ReviewsNew.vue
@@ -1,86 +1,41 @@
 <template>
-  <div class="container">
-    <div class="divide50"></div>
-    <div class="reviews-new">
-      <h2>Rate Professor {{ professor.name }}</h2>
-      <form v-on:submit.prevent="createReview()">
-        <ul>
-          <li class="text-danger" v-for="error in errors">{{ error }}</li>
-        </ul>
-        <div class="form-group">
-          <label>Course Code:</label>
-          <input type="text" class="form-control" v-model="courseCode" />
-        </div>
-        <div class="form-group">
-          <label>Title:</label>
-          <input type="text" class="form-control" v-model="title" />
-        </div>
-          <div class="stars">
-          <label>Rating:</label>
-          <form action="">
-            <input class="star star-5" id="star-5" value="5" type="radio" v-model="rating" name="star"/>
-            <label class="star star-5" for="star-5"></label>
-            <input class="star star-4" id="star-4" value="4" type="radio" v-model="rating" name="star"/>
-            <label class="star star-4" for="star-4"></label>
-            <input class="star star-3" id="star-3" value="3" type="radio" v-model="rating" name="star"/>
-            <label class="star star-3" for="star-3"></label>
-            <input class="star star-2" id="star-2" value="2" type="radio" v-model="rating" name="star"/>
-            <label class="star star-2" for="star-2"></label>
-            <input class="star star-1" id="star-1" value="1" type="radio" v-model="rating" name="star"/>
-            <label class="star star-1" for="star-1"></label>
-          </form><br>
-        </div><br>
-        <div class="form-group">
-          <label>Review:</label>
-          <input
-            type="text"
-            class="form-control"
-            id="reviewtxtarea"
-            v-model="review"
-          />
-        </div><br>
-      
-        <input type="submit" class="btn btn-primary" value="Submit" />
-      </form>
-    </div>
-    <div class="divide50"></div>
+  <div class="reviews-new">
+    <h1>Rate Professor {{ professor.name }}</h1>
+    <form v-on:submit.prevent="createReview()">
+      <ul>
+        <li class="text-danger" v-for="error in errors">{{ error }}</li>
+      </ul>
+      <div class="form-group">
+        <label>Course Code:</label>
+        <input type="text" class="form-control" v-model="courseCode" />
+      </div>
+      <div class="form-group">
+        <label>Title:</label>
+        <input type="text" class="form-control" v-model="title" />
+      </div>
+      <div class="form-group">
+        <label>Review:</label>
+        <input
+          type="text"
+          class="form-control"
+          id="reviewtxtarea"
+          v-model="review"
+        />
+      </div>
+      <div class="form-group">
+        <label>Rating:</label>
+        <input type="number" class="form-control" v-model="rating" />
+      </div>
+      <br />
+      <input type="submit" class="btn btn-primary" value="Submit" />
+    </form>
   </div>
 </template>
 
 <style>
 #reviewtxtarea {
+  width: 500px;
   height: 100px;
-}
-div.stars {
-  width: 270px;
-  display: inline-block;
-}
- 
-input.star { display: none; }
- 
-label.star {
-  float: right;
-  padding: 10px;
-  font-size: 36px;
-  color: rgb(132, 130, 130);
-  transition: all .2s;
-}
- 
-input.star:checked ~ label.star:before {
-  content: '\f005';
-  color: #FD4;
-  transition: all .25s;
-}
- 
-input.star-5:checked ~ label.star:before {
-  color: #FE7;
-}
- 
-input.star-1:checked ~ label.star:before { color: #F62; }
- 
-label.star:before {
-  content: '\f006';
-  font-family: FontAwesome;
 }
 </style>
 
